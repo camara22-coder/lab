134ed644122db54a6e3221e8693328c2386cf303
camara22-coder <https://github.com/camara22-coder/lab.git>
2025-01-12 16:28:47 +0000

    calcule age

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("hello world")
+//alert("hello world")
+let date_de_naissance=prompt("entrez date de naissance:")
+let nouvel_annee=prompt("entrez la nouvelle année:")
+let age=parseInt(nouvel_annee) - parseInt(date_de_naissance)
+
+if (isNaN(age)) {
+	alert("veillez entrez une année valide!")
+}else{
+	alert("vous avez "+ age + " ans ")
+}
